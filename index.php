8848f5774adab6bbc63d2dab850db5563728d5c0
Angel Campos <campos.angel@student.greenriver.edu>
2022-05-19 13:53:53 -0400

    updated index

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ error_reporting(E_ALL);
 
 //Require the necessary autoload file
 require_once('vendor/autoload.php');
-require_once ('model/data-layer.php');
-require_once ('model/validation.php');
-require_once ('classes/order.php');
+//require_once ('model/data-layer.php'); //don't need after added to composer.json
+//require_once ('model/validation.php'); //don't need after added to composer.json
+//require_once ('classes/order.php'); //don't need after added to composer.json
 
 //Start a session-has to come before any HTML OUTPUT
 session_start();
@@ -22,34 +22,33 @@ var_dump($order);*/
 
 //Create an instance of the Base Class
 $f3 = Base::instance();
-//echo gettype($f3);
+
+//Create an instance of the Controller class
+$con = new Controller($f3);
 
 //Define a default route
-$f3->route('GET|POST /', function()
-{
-    $view = new Template();
-    echo $view->render('views/home.html');
-    //echo '<h1>My Diner 2</h1>';
+$f3->route('GET|POST /', function() {
+    //global $con;
+    //$con->home();
+    //or
+    $GLOBALS['con']->home();
+
+    //$view = new Template();
+    //echo $view->render('views/home.html');
 }
 );
 
 //Define a breakfast route
-$f3->route('GET|POST /breakfast', function()
-{
+$f3->route('GET|POST /breakfast', function() {
     $view = new Template();
     echo $view->render('views/breakfast.html');
-    //echo '<h1>Breakfast Page</h1>';
-
 }
 );
 
 //Define a lunch route
-$f3->route('GET|POST /lunch', function()
-{
+$f3->route('GET|POST /lunch', function() {
     $view = new Template();
     echo $view->render('views/lunch.html');
-    //echo '<h1>Breakfast Page</h1>';
-
 }
 );
 
@@ -64,117 +63,19 @@ $f3->route('GET|POST /dinner', function()
 );
 
 //Define a order route
-$f3->route('GET|POST /order', function($f3)
-{
-    //if the form has been submitted
-    if($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
-        //get food from POST array
-        $food = $_POST['food'];
-        $f3->set('userFood', $food);
-
-        //get meal from POST array
-        //option 1
-        $meal = "";
-        if(isset($_POST['meal']))
-        {
-            $meal = $_POST['meal'];
-        }
-
-        //option 2
-        //$meal = isset($_POST['meal']) ? $_POST['meal'] : "";
-
-        //add the users meal to the hive
-        $f3->set('userMeal', $meal);
-
-        //if data is valid
-        if(validFood($food))
-        {
-            //create a bew Irder object
-            $order = new Order();
-
-            //Add the food to the order
-            $order->setFood($food);
-
-            //store the order in session array
-            $_SESSION['order'] = $order;
-            //$_SESSION['food'] = $food;
-        }
-        //if data is not valid -> store an error message
-        else {
-            $f3->set('errors["food"]', 'Please enter a food, at least 2 characters');
-        }
-
-        if (validMeal($meal)) {
-            //store in session array
-            $_SESSION['order']->setMeal($meal);
-            //$_SESSION['meal'] = $meal;
-            //$_SESSION['meal'] = $_POST['meal'];
-        }
-        //if data is not valid -> store an error message
-        else {
-            $f3->set('errors["meal"]', 'Please select a meal');
-        }
-
-        //redirect to order2 route if there are no errors
-        if (empty($f3->get('errors'))) {
-            header('location: order2');
-        }
-    }
-
-    //add meal data to hive
-    $f3->set('meals', getMeals());
-
-    $view = new Template();
-    echo $view->render('views/orderForm1.html');
-    //echo '<h1>Order Page</h1>';
-}
-);
+$f3->route('GET|POST /order', function($f3) {
+    $GLOBALS['con']->order();
+});
 
 //Define a order2 route
 $f3->route('GET|POST /order2', function($f3) {
-    //var_dump ($_POST);
-
-    //add condiment data to hive
-    $f3->set('condiments', getCondiments());
-
-    if($_SERVER['REQUEST_METHOD'] == 'POST')
-    {
-        $conds = "";
-        if(empty($_POST['conds'])) {
-            $conds = "none selected";
-        }
-        else {
-            $conds = implode(", ", $_POST['conds']);
-        }
-        $_SESSION['order']->setCondiments($conds);
-        //$_SESSION['conds'] = $conds;
-
-        header("location: summary");
-    }
-
-    $view = new Template();
-    echo $view->render('views/orderForm2.html');
-    //echo '<h1>Order Page</h1>';
+    $GLOBALS['con']->order2();
 }
 );
 
 //Define a order2 route
 $f3->route('POST|GET /summary', function() {
-    //var_dump ($_POST);
-    /*echo "<pre>";
-    var_dump($_SESSION);
-    echo "</pre>";*/
-
-    //before changing to above if statement:
-    //$_SESSION['conds'] = implode(", ", $_POST['conds']);
-
-    $view = new Template();
-    echo $view->render('views/orderSummary.html');
-    //echo '<h1>Order Page</h1>';
-
-    //end session
-    //session_destroy();
+    $GLOBALS['con']->summary();
 }
 );
 
